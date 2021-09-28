bacaab1e3b60b48ed7a879dd3d09c7a8c1e50808
Daniel Miller <millerpils@gmail.com>
2021-09-28 12:07:26 +0100

    Updates

--- restaurants_api/routes/restaurants.js
+++ restaurants_api/routes/restaurants.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Router = express.Router();
-const { Restaurant, Menu } = require('../models/models');
+const { Restaurant } = require('../models/models');
 
 // CREATE
 Router.post('/', async (req, res) => {
